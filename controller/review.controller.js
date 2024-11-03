1b4f8167f7ce80238f81388100e687310d932d35
abir <antorabir@gmail.com>
2024-11-03 18:40:04 +0600

    add: review route and controller

---
+++ controller/review.controller.js
@@ -0,0 +1,260 @@
+const mongoose = require("mongoose");
+const { validationResult } = require("express-validator");
+const { failure, success } = require("../utilities/common");
+const HTTP_STATUS = require("../constants/statusCodes");
+const Review = require("../model/review.model");
+const Property = require("../model/property.model");
+const User = require("../model/user.model");
+const addReviewToProperty = async (req, res) => {
+  try {
+    // const validation = validationResult(req).array();
+    // // console.log(validation);
+    // if (validation.length > 0) {
+    //   return res
+    //     .status(HTTP_STATUS.OK)
+    //     .send(failure("Failed to add the review", validation[0].msg));
+    // }
+    if (!req.user || !req.user._id) {
+      return res.status(HTTP_STATUS.OK).send(failure("please login first"));
+    }
+    const authId = req.user._id;
+
+    const { propertyId, review, rating } = req.body;
+
+    if (!propertyId || !review || !rating) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("Please provide all the required fields"));
+    }
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("Property not found"));
+    }
+
+    const propertyReview = await Review.findOne({
+      property: propertyId,
+      user: authId,
+    });
+
+    if (propertyReview) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("Same user cannot review twice"));
+    }
+
+    console.log("Creating new review...");
+    const newReview = await Review.create({
+      user: authId,
+      property: propertyId,
+      review: review,
+      rating: rating,
+    });
+    // Update property with new review
+    if (!newReview) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("Failed to add the review"));
+    }
+
+    property.reviews.push(newReview._id);
+
+    console.log(property.totalRatings, property.averageRating);
+
+    property.totalRatings += 1;
+    property.averageRating =
+      (property.averageRating * (property.totalRatings - 1) + rating) /
+      property.totalRatings;
+
+    await property.save();
+
+    return res
+      .status(HTTP_STATUS.CREATED)
+      .send(success("Review added successfully", newReview));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .send(failure("Internal server error"));
+  }
+};
+
+const addReviewToWebsite = async (req, res) => {
+  try {
+    const { review, rating, userId } = req.body;
+
+    if (!review || !rating || !userId) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .send(failure("All fields are required"));
+    }
+
+    const reviewExists = await Review.findOne({ userId });
+
+    if (reviewExists) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("Same user cannot review twice"));
+    }
+
+    const user = await User.findById({ _id: userId });
+
+    if (!user) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("User with ID was not found"));
+    }
+
+    const newReview = await Review.create({
+      userId,
+      review,
+      rating,
+    });
+    if (newReview) {
+      return res
+        .status(HTTP_STATUS.CREATED)
+        .send(success("Added review successfully", newReview));
+    }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .send(failure("Internal server error"));
+  }
+};
+
+const getAllWebsiteReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find({});
+
+    if (reviews) {
+      return res
+        .status(HTTP_STATUS.OK)
+        .send(success("all reviews fetched successfully", reviews));
+    }
+    return res
+      .status(HTTP_STATUS.OK)
+      .send(failure("reviews could not be fetched"));
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(`internal server error`);
+  }
+};
+
+const getReviewByReviewId = async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+
+    const review = await Review.findOne({ _id: reviewId });
+    if (!review) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("review could not be fetched"));
+    }
+    return res
+      .status(HTTP_STATUS.OK)
+      .send(success("review fetched successfully", review));
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(`internal server error`);
+  }
+};
+
+const getReviewByUserId = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .send(failure("All fields are required"));
+    }
+
+    const review = await Review.findOne({ userId });
+    if (!review) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("review could not be fetched"));
+    }
+    return res
+      .status(HTTP_STATUS.OK)
+      .send(success("review fetched successfully", review));
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(`internal server error`);
+  }
+};
+
+const editReview = async (req, res) => {
+  try {
+    const { review, rating, userId } = req.body;
+    const { reviewId } = req.params;
+
+    if (!reviewId || !userId) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .send(failure("reviewId and userId are required"));
+    }
+
+    const reviewExists = await Review.findOne({
+      _id: reviewId,
+      userId: userId,
+    });
+
+    if (!reviewExists) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("review does not exist"));
+    }
+
+    reviewExists.review = review || reviewExists.review;
+    reviewExists.rating = rating || reviewExists.rating;
+    await reviewExists.save();
+    return res
+      .status(HTTP_STATUS.OK)
+      .send(success("review updated successfully", reviewExists));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .send(failure("Internal server error"));
+  }
+};
+
+const deleteReview = async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    if (!reviewId) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .send(failure("reviewId is required"));
+    }
+    const deletedReview = await Review.findByIdAndDelete({ _id: reviewId });
+    if (!deletedReview) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("review could not be deleted"));
+    }
+    return res
+      .status(HTTP_STATUS.OK)
+      .send(success("review deleted successfully", deletedReview));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .send(failure("Internal server error"));
+  }
+};
+
+module.exports = {
+  addReviewToProperty,
+  addReviewToWebsite,
+  getAllWebsiteReviews,
+  getReviewByUserId,
+  getReviewByReviewId,
+  editReview,
+  deleteReview,
+};
---
+++ routes/review.route.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const routes = express();
+const { reviewValidator, userValidator } = require("../middleware/validation");
+const {
+  addReviewToProperty,
+  editReview,
+  getAllWebsiteReviews,
+  getReviewByReviewId,
+  getReviewByUserId,
+  deleteReview,
+} = require("../controller/review.controller");
+// const { isAuthenticated, isAdmin } = require("../middleware/auth");
+const { isAuthorizedUser } = require("../middleware/authValidationJWT");
+// const reviewValidator = require("../middleware/reviewValidation")
+
+routes.get("/all-reviews", getAllWebsiteReviews);
+routes.get("/review-by-user", getReviewByUserId);
+routes.get("/get-one-review/:reviewId", getReviewByReviewId);
+
+routes.post(
+  "/add-review",
+  //   isAuthorizedUser,
+  //   reviewValidator.addReview,
+  isAuthorizedUser,
+  addReviewToProperty
+);
+
+routes.delete(
+  "/delete-review/:reviewId",
+  //   isAuthorizedUser,
+  //   userValidator.delete,
+  deleteReview
+);
+
+routes.put(
+  "/update-review/:reviewId",
+  //   isAuthorizedUser,
+  //   reviewValidator.updateReview,
+  editReview
+);
+
+module.exports = routes;
